bf26fb8af255b3184cd6d68a2ca68a3874e07166
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-29 02:09:46 +0530

    Added stuff to main.py
    Import modules on separate lines
    
    Used a descriptive name for the trainer

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ from capsgnn import CapsGNNTrainer
 from param_parser import parameter_parser
 
 def main():
-
+    # Parse command line arguments
     args = parameter_parser()
+
+    # Print table of parameters
     tab_printer(args)
-    model = CapsGNNTrainer(args)
-    model.fit()
-    model.score()
-    model.save_predictions()
+
+    # Initialize CapsGNN trainer
+    trainer = CapsGNNTrainer(args)
+
+    # Fit the model
+    trainer.fit()
+
+    # Evaluate the model
+    trainer.score()
+
+    # Save model predictions
+    trainer.save_predictions()
 
 if __name__ == "__main__":
     main()
