937bf1d41f65b3413486ff74f6af98b60e241ddf
Harsh Bishnoi <112414164+harsh2929@users.noreply.github.com>
2023-03-29 02:05:25 +0530

    Updated fxcn.py
    Added docstrings to functions
    
    Used list comprehension to create table rows

--- fxcn.py
+++ fxcn.py
@@ -14,3 +14,16 @@ def tcb(args):
 def cmc(node_properties):
 
     return {value:i for i, value in enumerate(node_properties)}
+'''
+def tcb(args):
+    """
+    Prints a table with the parameter names and their values.
+    """
+    args = vars(args)
+    keys = sorted(args.keys())
+    t = Texttable() 
+    t.add_rows([["Parameter", "Value"]])
+    t.add_rows([[f"{k.replace('_', ' ').capitalize()}", args[k]] for k in keys])
+    print(t.draw())
+
+'''
